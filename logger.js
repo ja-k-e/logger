5c8cf36da608f2be23cf02ae4d793c97e678b902
Jake Albaugh <jake.albaugh@gmail.com>
2015-06-25 19:01:40 -0500

    removed examples from source

--- logger.js
+++ logger.js
@@ -1,24 +1,3 @@
-// 
-// set up a new logger
-//
- 
-var log = new Logger();
- 
- 
- 
-//
-// example usage
-// 
- 
-log.info("Hey, here’s some ‘info’, the number 2 is equal to:", 2);
-log.pass("Great Job! You ‘pass’!");
-log.warn("Watch yourself. I will only ‘warn’ you once...");
-log.fail("Woah, bro! Total ‘fail’! This following object is totally not what you were looking for.", { awesome: false } );
-log.go("Let’s just ‘go’ with something a bit more basic.");
-log.info([0,1,2,3,4,5], { more: 'data'} );
- 
- 
- 
 //
 // the goods
 //
